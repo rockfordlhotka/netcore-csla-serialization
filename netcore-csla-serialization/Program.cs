15d68857e901ba93537069b9657532755d022919
Rockford Lhotka <rocky@lhotka.net>
2017-10-04 19:52:37 -0500

    Add project files.

---
+++ netcore-csla-serialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netcore_csla_serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
